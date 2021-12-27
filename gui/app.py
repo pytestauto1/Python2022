9668942a4d83ef8e89648ed0c0b75d76a76285fa
pytestauto1 <linussebastion@gmail.com>
2021-12-27 17:11:26 +0000

    frames

--- gui/app.py
+++ gui/app.py
@@ -7,4 +7,8 @@ root = tk.Tk()
 
 canvas = tk.Canvas(root, height=600, width=600, bg="#263D42")
 canvas.pack()
+
+frame = tk.Frame(root,bg="white")
+frame.place(relheight=0.8,relwidth=0.8,relx=0.1,rely=0.1)
+
 root.mainloop()
