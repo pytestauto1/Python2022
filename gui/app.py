a1d25eea391a7ab0fca0034f5dfc45d00ee61479
pytestauto1 <linussebastion@gmail.com>
2021-12-27 17:18:06 +0000

    open dialog, select exe or all files

--- gui/app.py
+++ gui/app.py
@@ -5,7 +5,8 @@ import os
 
 root = tk.Tk()
 
-
+def addApp():
+    filename = filedialog.askopenfilename(initialdir="/", title = "Select a file", filetypes=(("Executable", "*.exe"),("All files","*.*")))
 
 canvas = tk.Canvas(root, height=600, width=600, bg="#263D42")
 canvas.pack()
@@ -15,7 +16,7 @@ frame.place(relheight=0.8,relwidth=0.8,relx=0.1,rely=0.1)
 
 
 #add buttons
-openFile = tk.Button(root,text="Open File", padx=10, pady=10, fg="white" ,bg="#263D42")
+openFile = tk.Button(root,text="Open File", padx=10, pady=10, fg="white" ,bg="#263D42", command=addApp)
 openFile.pack()
 
 runApps = openFile = tk.Button(root,text="Run Apps", padx=10, pady=10, fg="white" ,bg="#263D42")
