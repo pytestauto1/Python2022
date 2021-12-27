778c01456c9d9d53043574a8b3072ceff79f5150
pytestauto1 <linussebastion@gmail.com>
2021-12-27 18:16:39 +0000

    gets lrc price, updates every 1000ms

--- cryptopricetracker/main.py
+++ cryptopricetracker/main.py
@@ -1,1 +1,30 @@
 import requests
+import tkinter as tk 
+from datetime import datetime
+
+
+def tracklrc():
+    url = "https://min-api.cryptocompare.com/data/price?fsym=LRC&tsyms=USD"
+    response = requests.get(url).json()
+    price = response["USD"]
+    
+    time = datetime.now().strftime("%H:%M:%S")
+
+    labelPrice.config(text = str(price) + "$")
+    labelTime.config(text = "updated at:  " + time )
+
+    canvas.after(1000, tracklrc)
+
+canvas = tk.Tk()
+canvas.geometry("400x500")
+canvas.title("Loopring  Tracker")
+
+labelPrice = tk.Label(canvas, text = "Loopring price:")
+labelPrice.pack(pady=20)
+
+labelTime = tk.Label(canvas)
+labelTime.pack(pady=20)
+
+
+tracklrc()
+canvas.mainloop()
