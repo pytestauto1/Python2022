a58b6937cdc4b3e07a284075663698765ff28545
pytestauto1 <linussebastion@gmail.com>
2021-12-27 17:15:09 +0000

    buttons added

--- gui/app.py
+++ gui/app.py
@@ -5,10 +5,20 @@ import os
 
 root = tk.Tk()
 
+
+
 canvas = tk.Canvas(root, height=600, width=600, bg="#263D42")
 canvas.pack()
 
 frame = tk.Frame(root,bg="white")
 frame.place(relheight=0.8,relwidth=0.8,relx=0.1,rely=0.1)
+
+
+#add buttons
+openFile = tk.Button(root,text="Open File", padx=10, pady=10, fg="white" ,bg="#263D42")
+openFile.pack()
+
+runApps = openFile = tk.Button(root,text="Run Apps", padx=10, pady=10, fg="white" ,bg="#263D42")
+runApps.pack()
 
 root.mainloop()
