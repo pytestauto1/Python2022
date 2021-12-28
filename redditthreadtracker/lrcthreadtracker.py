822bcd15c2dbac6615e1e4afe6619a136358c2e8
pytestauto1 <linussebastion@gmail.com>
2021-12-28 18:10:59 +0000

    get 1st new thread function works , stores

--- redditthreadtracker/lrcthreadtracker.py
+++ redditthreadtracker/lrcthreadtracker.py
@@ -13,16 +13,27 @@ listing = 'new'
 
 
 #api query look for new thread in subreddit
-def newLrcThread():
+def newLrcThread(subreddit,listing,limit,timeframe):
     try:
         url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
         request = requests.get(url,headers = {'User-agent':'lrccheck'})
-        print(request.json())
+        #print(request.json())
     except:
         print('error')
     return request.json()
-    
-    
+
+r = newLrcThread(subreddit,listing,limit,timeframe)
+print(r)  
+
+""" def newLrcThreadTitle(r):
+    posts = []
+    for post in r['data']['children']:
+        x = post['data']['title']
+        posts.append(x)
+
+posts = newLrcThreadTitle(r)
+print(posts) """
+
 
     # response = requests.get(url)
     # print(response)
@@ -43,7 +54,7 @@ canvas.title("Reddit-Loopring-LRC")
 
 # a layer to return values
 
-newLrcThread()
+newLrcThread(subreddit,listing,limit,timeframe)
 
 
 
